ca9b99e3390969dd0eee7370f835e646eb5f1250
Heloise Taillet <ht2394@columbia.edu>
2019-01-02 19:00:12 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,18 +5,18 @@ end
 
 def second_challenge
   epic_tragedy = {
-    montague: {
+    montague: [
       patriarch: {},
       matriarch: {},
       hero: {},
       hero_friends: []
-    },
-    capulet: {
+    ],
+    capulet: [
       patriarch: {},
       matriarch: {},
       heroine: {},
       heroine_friends: []
-    }
+    ]
   }
 end
 
